b4d8346b7c5668f54af82502f1adef39a28122e4
Preethish Kumar <preethish.kumar@philips.com>
2021-09-22 17:02:36 +0530

    Filename appended with traits

--- SVG Creator/Program.cs
+++ SVG Creator/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SVG_Creator  {
@@ -8,46 +10,70 @@ namespace SVG_Creator  {
     public class Program  {
         public static async Task Main() {
             int count = 0;
+            StringBuilder filename= new StringBuilder();
             // Iterate thru all Traits and merge the SVG using Gene 
             // TODO consider Traits.Avatar and other attributes
-            foreach (var bgColor in Constants.bgColorMap) {
+            foreach (var bgColor in Constants.bgColorMap)
+            {
+                filename.Append(bgColor.Value.Name);
                 // First Create Background with trait color
-                string backgroundRectWithColor = Constants.backgroundRect.Replace(Constants.bgColorFill, bgColor.Value.SVG);
-                foreach (var skinColor in Constants.skinColorMap) {
+                var backgroundRectWithColor =
+                    Constants.backgroundRect.Replace(Constants.bgColorFill, bgColor.Value.SVG);
+                foreach (var skinColor in Constants.skinColorMap)
+                {
                     // Add Skin with trait color
-                    string skinWithColor = Constants.skin.Replace(Constants.skinColorFill, skinColor.Value.SVG);
+                    var skinWithColor = Constants.skin.Replace(Constants.skinColorFill, skinColor.Value.SVG);
+                    filename.Append(skinColor.Value.Name);
                     // Add the Crown
-                    foreach (var crownmap in Constants.crownMap) {
+                    foreach (var crownmap in Constants.crownMap)
+                    {
+                        filename.Append(crownmap.Value.Name);
                         // Add the face with skin color
-                        foreach (var faceMap in Constants.faceMap) {
-                        string faceWithColor = faceMap.Value.SVG.Replace(Constants.skinColorFill, skinColor.Value.SVG);
+                        foreach (var faceMap in Constants.faceMap)
+                        {
+                            var faceWithColor = faceMap.Value.SVG.Replace(Constants.skinColorFill, skinColor.Value.SVG);
+                            filename.Append(faceMap.Value.Name);
                             //Add the dress
-                            foreach (var dressMap in Constants.dressMap) {
+                            foreach (var dressMap in Constants.dressMap)
+                            {
+                                filename.Append(dressMap.Value.Name);
                                 // Add the Weapon
-                                foreach (var weapon in Constants.weaponMap) {
+                                foreach (var weapon in Constants.weaponMap)
+                                {
                                     //Add the hand cover
-                                    string handCoverWithSkin =
+                                    var handCoverWithSkin =
                                         Constants.handCover.Replace(Constants.skinColorFill, skinColor.Value.SVG);
+                                    filename.Append(weapon.Value.Name);
                                     // Combine all and create the final art work
                                     // Update the file location here
-                                    File.WriteAllText("C:\\work\\NFT\\SVG\\Output\\Dummy" + count + ".svg",
-                                    Constants.svgHeader +
-                                    backgroundRectWithColor +
-                                    skinWithColor +
-                                    Constants.hair +
-                                    crownmap+
-                                    dressMap.Value.SVG +
-                                    faceWithColor +
-                                    weapon.Value.SVG +
-                                    handCoverWithSkin +
-                                    Constants.svgFooter
-                                );
-                                count++;
+                                    File.WriteAllText("C:\\work\\NFT\\SVG\\Output\\Dummy" + count + filename + ".svg",
+                                        Constants.svgHeader +
+                                        backgroundRectWithColor +
+                                        skinWithColor +
+                                        Constants.hair +
+                                        crownmap.Value.SVG +
+                                        dressMap.Value.SVG +
+                                        faceWithColor +
+                                        weapon.Value.SVG +
+                                        handCoverWithSkin +
+                                        Constants.svgFooter
+                                    );
+                                    count++;
+                                    filename.Replace(weapon.Value.Name, "");
+                                }
+
+                                filename.Replace(dressMap.Value.Name, "");
                             }
+                            filename.Replace(faceMap.Value.Name,"");
                         }
+
+                        filename.Replace(crownmap.Value.Name, "");
                     }
-                    }
+
+                    filename.Replace(skinColor.Value.Name, "");
                 }
+
+                filename.Replace(bgColor.Value.Name, "");
             }
         }
     }
